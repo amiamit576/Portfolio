63cd042028a4b107011aae9c80c2135819358ec0
KunalSingh2202 <kunnu2202@gmail.com>
2025-02-05 21:55:39 +0530

    header added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,11 @@
+import Header from "./components/Header";
+
 function App() {
   return (
-    <h1 className="text-3xl text-amber-300">Hello wolrd!</h1>
-  )
+   <>
+    <Header/>
+   </>
+  );
 }
 
-export default App
+export default App;
