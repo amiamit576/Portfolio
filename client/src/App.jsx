ef8b571a919a5deef24acc88bfea41ff009bcd26
Kunal Singh <126372340+KunalSingh2202@users.noreply.github.com>
2025-02-10 10:47:47 +0530

    Update App.jsx
    QuickContact added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import Header from "./layout/Header";
 import Footer from "./layout/Footer";
+import QuickContact from "./components/QuickContact";
 
 function App() {
   return (
    <>
     <Header/>
-    
+     <QuickContact/>
     <Footer/>
    </>
   );
